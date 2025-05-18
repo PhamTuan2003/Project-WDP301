153233611f916ad241d714ddcc66c81c33c74906
Nguyen Duc Huy <huyndhe176876@fpt.edu.vn>
2025-05-19 00:56:54 +0700

    Yacht

--- controller/yachtController.js
+++ controller/yachtController.js
@@ -1,60 +1,50 @@
-const Yacht = require('../model/yachtSchema');
-const {location, company, yachtType, yacht } = require('../model');
-const cloudinary = require('../utils/configClound')
+const { location, company, yachtType, Yacht } = require("../model");
+const cloudinary = require("../utils/configClound");
 
 // hàm tao mới một chiếc du thuyền
 const createYacht = async (req, res) => {
-    try {
-        const { name, launch, description, hullBody, rule, itinerary, location_id, yachtType_id, id_companys } = req.body;
-        let imageUrt = '';
+  try {
+    const { name, launch, description, hullBody, rule, itinerary, location_id, yachtType_id, id_companys } =
+      req.body;
+    let imageUrt = "";
 
-        if (req.file) {
-            const result = await cloudinary.uploader.upload_stream(
-                { folder: 'yachts' },
-                (error, result) => {
-                    if (error) {
-                        console.error('Error uploading image:', error);
-                        return res.status(500).json({ message: 'Error uploading image' });
-                    }
-                    imageUrt = result.secure_url;
-
-                    const yacht = new Yacht({
-                        name,
-                        image: imageUrt,
-                        launch,
-                        description,
-                        hullBody,
-                        rule,
-                        itinerary,
-                        location_id,
-                        yachtType_id,
-                        id_companys
-                    });
-                    yacht.save()
-                        .then(() => {
-                            res.status(201).json(yacht);
-                        })
-                        .catch((error) => {
-                            console.error('Error saving yacht:', error);
-                            res.status(500).json({ message: 'Error saving yacht' });
-                        });
-                }
-            )
-            result.end(req.file.buffer);
-        } else {
-            res.status(400).json({ message: 'No image file provided' });
+    if (req.file) {
+      const result = await cloudinary.uploader.upload_stream({ folder: "yachts" }, (error, result) => {
+        if (error) {
+          console.error("Error uploading image:", error);
+          return res.status(500).json({ message: "Error uploading image" });
         }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+        imageUrt = result.secure_url;
+
+        const yacht = new Yacht({
+          name,
+          image: imageUrt,
+          launch,
+          description,
+          hullBody,
+          rule,
+          itinerary,
+          location_id,
+          yachtType_id,
+          id_companys,
+        });
+        yacht
+          .save()
+          .then(() => {
+            res.status(201).json(yacht);
+          })
+          .catch((error) => {
+            console.error("Error saving yacht:", error);
+            res.status(500).json({ message: "Error saving yacht" });
+          });
+      });
+      result.end(req.file.buffer);
+    } else {
+      res.status(400).json({ message: "No image file provided" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-
-module.exports = {
-    createYacht
-}
-
-
-
-
-
+module.exports = { createYacht };
--- model/yachtSchema.js
+++ model/yachtSchema.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const yachtSchema = new Schema({
-  name: String,
-  image: String,
-  launch: String,
-  description: String,
-  hullBody: Number,
-  rule: Number,
-  itinerary: String,
-  locationId: { type: mongoose.Schema.Types.ObjectId, ref: "Location" },
-  yachtTypeId: { type: mongoose.Schema.Types.ObjectId, ref: "YachtType" },
-  IdCompanys: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  isDeleted: { type: Boolean, default: true },
-});
+const yachtSchema = new Schema(
+  {
+    name: String,
+    image: String,
+    launch: String,
+    description: String,
+    hullBody: Number,
+    rule: Number,
+    itinerary: String,
+    locationId: { type: mongoose.Schema.Types.ObjectId, ref: "Location" },
+    yachtTypeId: { type: mongoose.Schema.Types.ObjectId, ref: "YachtType" },
+    IdCompanys: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
+    isDeleted: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Yacht", yachtSchema, "yachts");
