c54361231a47d7d14607f6d29ad97e3321623005
Thuc29 <112091905+Thuc29@users.noreply.github.com>
2025-05-27 17:53:19 +0700

    Fix YachtSchema

--- model/yachtSchema.js
+++ model/yachtSchema.js
@@ -9,6 +9,7 @@ const yachtSchema = new Schema(
     hullBody: String, //thân vỏ làm bằng gì
     rule: Number, //sức chứa hành khách
     itinerary: String, //lịch trình
+    price: Number, //giá thuê
     locationId: { type: mongoose.Schema.Types.ObjectId, ref: "Location" }, //địa điểm du thuyền hoạt động
     yachtTypeId: { type: mongoose.Schema.Types.ObjectId, ref: "YachtType" }, //loại du thuyền
     IdCompanys: { type: mongoose.Schema.Types.ObjectId, ref: "Company" }, //công ty sở hữu hoặc điều hành du thuyền
